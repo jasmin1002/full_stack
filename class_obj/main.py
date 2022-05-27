3b3363dd66cd804121364a37339e5e67fd09f72c
Jasmin1002 <jasmineolofin@gmail.com>
2022-05-27 22:39:23 +0100

    class and object instantiation worked on

---
+++ class_obj/main.py
@@ -0,0 +1,36 @@
+class Student:
+    # Constructor
+    def __init__(self, name="student", age=0, tracks=[], score=0.00):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score
+
+    # Modify existing name (i.e set new name)
+    def change_name(self, name):
+        self.name = name
+
+    # Modify age
+    def change_age(self, age):
+        self.age = age
+
+    # Print score
+    def get_score(self):
+        print('{:.2f}'.format(self.score))
+
+    # Add new entry
+    def add_track(self, track):
+        self.tracks.append(track)
+
+
+Bob = Student()
+
+# Expected methods
+print(Bob.__dict__)
+Bob.change_name("Peter")
+Bob.change_age(34)
+Bob.add_track("UI/UX")
+Bob.get_score()
+
+print(Bob.__dict__)
+
