768ee3e22eebbec7fa75f9bd1c737208bd15dd3b
Jasmine <jasmineolofin@gmail.com>
2022-06-01 10:08:26 +0000

    Setup file for django project first commit

---
+++ Environments/student_env/Sikiru_Jimoh/Sikiru_Jimoh/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Sikiru_Jimoh project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sikiru_Jimoh.settings')
+
+application = get_asgi_application()
