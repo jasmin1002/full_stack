ad22c20cc21ac54193275f87ae79e67c9a183cca
Jasmin1002 <jasmineolofin@gmail.com>
2022-06-01 18:27:41 +0100

    Added comments to line of code

--- RPS-Game/main.py
+++ RPS-Game/main.py
@@ -1,10 +1,12 @@
 import random
+# Game options
 options = {
     'S': 'Scissors',
     'R': 'Rock',
     'P': 'Paper'
 }
 
+# Initialize player and opponent choice
 def init(options={}):
     player_choice = input("Pick an option (S, R, P): ")
     
@@ -17,18 +19,21 @@ def init(options={}):
 
     return [player_choice.upper(), computer_choice]
 
-
+# Check for tie (same move between player and opponent)
 def isTie(player='', computer=''):
     if player == computer:
         print('Tie: the same moves\n')
         return True
 
+# Decide the winner between player and opponent
 def decide_winner(options=options, player='', opponent=''):
+    # Possible winning configuration
     winningCombo = [['R', 'S'], ['S', 'P'], ['P', 'R']]
 
     for combo in winningCombo:
         output = "{} beats {}\n{} wins."
         
+        # Select winner by R > S, S > P, or P > R
         if player in combo and opponent in combo:
             if combo.index(player) < combo.index(opponent):
                 output = output.format(options[player], options[opponent], 'Player')
@@ -38,14 +43,13 @@ def decide_winner(options=options, player='', opponent=''):
             print(output)
             break
 
-
+# Initialize both player and opponent
 player, computer = init(options)
 
+# Print players moves
 print(f"Player({options[player]}) : Computer({options[computer]})\n")
 if isTie(player, computer):
     player, computer = init(options)
 
-# Rock beats Scissors
-# Scissors beats Paper
-# Paper beats Rocks
+# Choose winner 
 decide_winner(options, player, computer)
