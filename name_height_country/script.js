aba1c7bfdb334c93cfc6fac293648ce843d99ea9
Jasmin1002 <jasmineolofin@gmail.com>
2022-06-23 03:15:25 +0100

    comment the line of code

--- name_height_country/script.js
+++ name_height_country/script.js
@@ -1,3 +1,4 @@
+// Define a generic Person class
 class Person {
     constructor(firstname='firstname', lastname='lastname', middlename='middlename', country='country', height=0.0) {
         this.firstname = firstname;
@@ -8,12 +9,16 @@ class Person {
     }
 }
 
+// Instantiate mySelf object
 const mySelf = new Person('Sikiru', 'JIMOH', 'Olalekan', 'Nigeria', 6.78)
 
-
+// Select output holder tag
 const container = document.querySelector('.container');
 
+// Destructure mySelf object
 const {lastname, firstname, middlename, country, height} = mySelf;
+
+// Wrap the values with appropriate html tags
 container.children[0].textContent = `Name: ${lastname} ${firstname} ${middlename}`;
 container.children[1].textContent = `Height: ${height}cm`
 container.children[2].textContent = `Country: ${country}`
